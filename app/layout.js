0b4a57cdcf259701dcea33d1f56a5bee0c214fe8
kelzerz <pointerthis228@gmail.com>
2025-10-22 23:28:39 +0300

    2

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,57 @@
+import "../styles/globals.css";
+
 export const metadata = {
 	title: "Мое PWA",
 	description: "Описание",
-	manifest: "/manifest.json"
+	manifest: "/manifest.json", // Путь к манифесту
+	themeColor: "#2b2b2b", // Цвет темы для PWA (можно изменить)
+	icons: [
+		{
+			rel: "icon",
+			href: "/icons/icon-192x192.png", // Иконка для браузера
+			sizes: "192x192"
+		},
+		// {
+		// 	rel: "apple-touch-icon",
+		// 	href: "/icons/icon-192x192.png", // Иконка для iOS
+		// 	sizes: "192x192"
+		// },
+		{
+			rel: "icon",
+			href: "/icons/icon-512x512.png", // Иконка для Android
+			sizes: "512x512"
+		}
+	]
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="ru">
+			<head>
+				{/* Мета теги пва */}
+				<link rel="manifest" href="/manifest.json" />
+				<meta name="theme-color" content="#2b2b2b" />
+				<meta name="apple-mobile-web-app-capable" content="yes" />
+				<meta
+					name="apple-mobile-web-app-status-bar-style"
+					content="black-translucent"
+				/>
+				<meta name="apple-mobile-web-app-title" content="Мое PWA" />
+
+				<link rel="icon" href="/icons/icon-192x192.png" sizes="192x192" />
+				{/* <link для эпл
+					rel="apple-touch-icon"
+					href="/icons/icon-192x192.png"
+					sizes="192x192"
+				/> */}
+
+				<meta name="mobile-web-app-capable" content="yes" />
+				<meta
+					name="msapplication-TileImage"
+					content="/icons/icon-192x192.png"
+				/>
+				<meta name="msapplication-TileColor" content="#2b2b2b" />
+			</head>
 			<body>{children}</body>
 		</html>
 	);
